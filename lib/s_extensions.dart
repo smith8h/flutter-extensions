0d66f502cb1de1151f5112e052ca8d1e3c5b966f
Hussein Shakir <husseinshakir81@gmail.com>
2025-11-11 01:20:42 +0300

    refactor(s_extensions): reorganize extension exports for better maintainability
    The exports were reordered to group related extensions together and maintain a consistent pattern. This improves code organization and makes it easier to locate specific extensions.

--- lib/s_extensions.dart
+++ lib/s_extensions.dart
@@ -1,8 +1,9 @@
 library s_extensions;
 
-export 'extensions/number_ext.dart';
-export 'extensions_context.dart';
-export 'extensions/string_ext.dart';
+export 'extensions/bool_ext.dart';
 export 'extensions/datetime_ext.dart';
-export 'extensions/map_ext.dart';
 export 'extensions/list_ext.dart';
+export 'extensions/map_ext.dart';
+export 'extensions/number_ext.dart';
+export 'extensions/string_ext.dart';
+export 'extensions_context.dart';
